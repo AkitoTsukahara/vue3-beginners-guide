703d738a5ecb5271f24944cf36e97fb04015e011
AkitoTsukahara <a-tsukahara@macloud.jp>
2021-09-05 22:40:19 +0900

    lesson component bind

--- chapter03/src/App.vue
+++ chapter03/src/App.vue
@@ -1,13 +1,20 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
+  <the-header v-bind:text="Math.floor(Math.random() * 10) % 2 === 0 ? 'even' : 'odd'"/>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
+
+<script lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue'
+
+export default {
+  components: {
+    HelloWorld,
+    TheHeader
+  }
+}
+</script>
 
 <style>
 #app {
---
+++ chapter03/src/components/TheHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>{{ text }}</header>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'TheHeader',
+    props: {
+        text: {
+            type: String,
+            default: 'kokoko'
+        }
+    }
+}
+</script>
+<style scoped>
+header {
+    width: 100%;
+    height: 60px;
+    text-align: center;
+    background: cornflowerblue;
+}
+</style>
